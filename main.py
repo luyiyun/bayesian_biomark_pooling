feat: control the randomness and fix the diagonal zero problem of hessian matrix in newtow iteration

main.py
@@ -25,7 +25,7 @@ def plot_params_hist(params_hist: np.ndarray, names: np.ndarray, savefn: str):
     fig.savefig(savefn)
 
 
-def temp_test_continue(ci=False, ve_method="bootstrap"):
+def temp_test_continue(ci=False, ve_method="bootstrap", seed=0):
     log_level = logging.WARNING
     logger = logging.getLogger("EMBP")
     logger.setLevel(log_level)
@@ -37,12 +37,13 @@ def temp_test_continue(ci=False, ve_method="bootstrap"):
     os.makedirs(root, exist_ok=True)
 
     simulator = Simulator(type_outcome="continue")
-    df = simulator.simulate()
+    df = simulator.simulate(seed)
     model = EMBP(
         outcome_type="continue",
         variance_estimate=ci,
         variance_estimate_method=ve_method,
         pbar=True,
+        seed=seed
     )
     model.fit(df["X"].values, df["S"].values, df["W"].values, df["Y"].values)
 
@@ -65,8 +66,8 @@ def temp_test_continue(ci=False, ve_method="bootstrap"):
     print(params)
 
 
-def temp_test_binary(ci=False):
-    log_level = logging.INFO
+def temp_test_binary(ci=False, seed=0):
+    log_level = logging.WARNING
     logger = logging.getLogger("EMBP")
     logger.setLevel(log_level)
     for handler in logger.handlers:
@@ -77,7 +78,7 @@ def temp_test_binary(ci=False):
     os.makedirs(root, exist_ok=True)
 
     simulator = Simulator(type_outcome="binary")
-    df = simulator.simulate()
+    df = simulator.simulate(seed)
     model = EMBP(
         outcome_type="binary",
         variance_estimate=ci,
@@ -86,6 +87,7 @@ def temp_test_binary(ci=False):
         pbar=True,
         n_importance_sampling=1000,
         use_gpu=True,
+        seed=seed
     )
     model.fit(df["X"].values, df["S"].values, df["W"].values, df["Y"].values)
 

src/bayesian_biomarker_pooling/embp.py
@@ -762,6 +762,7 @@ class BinaryEM(EM):
         pbar: bool = True,
         lr: float = 1.0,
         n_importance_sampling: int = 1000,
+        seed: int | None | Generator = None,
     ) -> None:
         super().__init__(
             max_iter=max_iter,
@@ -774,6 +775,8 @@ class BinaryEM(EM):
             delta2_var=delta2_var,
             pbar=pbar,
         )
+        # 如果是Generator，则default_rng会直接返回它自身
+        self._seed = np.random.default_rng(seed)
         self._lr = lr
         self._nIS = n_importance_sampling
 
@@ -878,7 +881,9 @@ class BinaryEM(EM):
         norm_lap = norm(loc=Xm, scale=1 / np.sqrt(hessian))
 
         # 进行IS采样
-        self._XIS = norm_lap.rvs(size=(self._nIS, self._n_m))  # N x n_m
+        self._XIS = norm_lap.rvs(
+            size=(self._nIS, self._n_m), random_state=self._seed
+        )  # N x n_m
 
         # 计算对应的(normalized) importance weights
         pIS = log_expit(
@@ -982,11 +987,10 @@ def bootstrap_estimator(
     Z: ndarray | None = None,
     Y_type: Literal["continue", "binary"] = False,
     n_repeat: int = 200,
-    seed: int | None | Generator = 0,
+    seed: int | None | Generator = None,
     pbar: bool = True,
 ) -> pd.DataFrame:
-    if not isinstance(seed, Generator):
-        seed = np.random.default_rng(seed)
+    seed = np.random.default_rng(seed)
 
     Svals = np.unique(S)
     is_m = pd.isnull(X)
@@ -1051,7 +1055,7 @@ class EMBP(BiomarkerPoolBase):
         variance_estimate_method: Literal["sem", "bootstrap"] = "sem",
         n_bootstrap: int = 200,
         pbar: bool = True,
-        seed: int | None = None,
+        seed: int | None = 0,
         use_gpu: bool = False,
     ) -> None:
         assert outcome_type in ["continue", "binary"]
@@ -1087,12 +1091,10 @@ class EMBP(BiomarkerPoolBase):
         self.var_est_method_ = variance_estimate_method
         self.n_bootstrap_ = n_bootstrap
         self.use_gpu_ = use_gpu
-        self.seed_ = seed
+        self.seed_ = np.random.default_rng(seed)
 
         if use_gpu and seed is not None:
             torch.random.manual_seed(seed)
-        else:
-            self._rng = np.random.default_rng(seed)
 
     def fit(
         self,
@@ -1143,6 +1145,7 @@ class EMBP(BiomarkerPoolBase):
                     pbar=self.pbar_,
                     lr=self.lr_,
                     n_importance_sampling=self.nIS_,
+                    seed=self.seed_
                 )
         self._estimator.register_data(X, S, W, Y, Z)
         self._estimator.run()
@@ -1163,11 +1166,13 @@ class EMBP(BiomarkerPoolBase):
                 Z=Z,
                 Y_type=self.outcome_type_,
                 n_repeat=self.n_bootstrap_,
-                seed=self._rng,
-                pbar=self.pbar_
+                seed=self.seed_,
+                pbar=self.pbar_,
             )
             res_ci = np.quantile(
-                res_bootstrap.values, q=[0.025, 0.975], axis=0,
+                res_bootstrap.values,
+                q=[0.025, 0.975],
+                axis=0,
             )
             self.params_["CI_1"] = res_ci[0, :]
             self.params_["CI_2"] = res_ci[1, :]

src/bayesian_biomarker_pooling/embp_gpu.py
@@ -55,8 +55,10 @@ def newton_raphson_beta(
     max_iter: int = 100,
     delta1: float = 1e-3,
     delta2: float = 1e-4,
+    epsilon: float = 1e-3,
 ):
     beta_ = init_beta
+    epsilon = torch.diag(torch.full_like(init_beta, epsilon))
     for i in range(max_iter):
         p_o = torch.sigmoid(Xo_des @ beta_)  # ns
         p_m = torch.sigmoid(Xm_des @ beta_)  # N x nm
@@ -72,7 +74,14 @@ def newton_raphson_beta(
         )
         H = H_o + H_m
 
-        beta_delta = lr * torch.inverse(H) @ grad
+        # 有时候可能会得到这样的H，其对角元会=0。这是因为某个beta_0s值非常大，导致
+        # p_o或p_m的值接近1（或者反过来），则(1-p)的值非常接近0，进而引发的问题。
+        if (torch.diag(H) < epsilon).any():
+            # 这里解决的办法是将算法替换为梯度下降法
+            beta_delta = lr * grad
+            tqdm.write(str(torch.diag(H)))
+        else:
+            beta_delta = lr * torch.inverse(H) @ grad
         beta_ -= beta_delta
 
         rdiff = (beta_delta.abs() / (beta_.abs() + delta1)).max()