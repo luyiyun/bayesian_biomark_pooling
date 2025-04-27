50dff98762ed7cea047b66ec661b2fe1caac8512
Luyiyun <18845728185@163.com>
2025-04-27 16:47:17 +0800

    add new tasks in run2.sh, remove the unuse codes in BBP/simulation.py

--- experiments/BBP/run2.sh
+++ experiments/BBP/run2.sh
@@ -15,3 +15,26 @@ for prev_i in "${prevalence[@]}"; do
             --betaz 2 3
     done
 done
+
+
+# Half Cauchy prior for sigma
+save_root="results/half_cauchy_prior"
+for prev_i in "${prevalence[@]}"; do
+    for OR_i in "${OR[@]}"; do
+        python ./simulation.py --prevalence $prev_i --OR $OR_i --nrepeat $nrepeat --ncores $ncores \
+            --nSamples $nSamples --n_knowX_balance --nKnowX $nKnowX \
+            --solver blackjax --save_root $save_root \
+            --prior_sigma_dist halfcauchy --prior_sigma_args 1.0
+    done
+done
+
+
+# imbalanced samples
+save_root="results/imbalanced_samples"
+for prev_i in "${prevalence[@]}"; do
+    for OR_i in "${OR[@]}"; do
+        python ./simulation.py --prevalence $prev_i --OR $OR_i --nrepeat $nrepeat --ncores $ncores \
+            --nSamples 200 150 100 50 --n_knowX_balance --nKnowX 20 15 10 5 \
+            --solver blackjax --save_root $save_root
+    done
+done
